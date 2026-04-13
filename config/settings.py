394c28cf299a442875f1b042cb145197575ac576
Amisha Gupta <amisha.gupta672@gmail.com>
2026-04-13 10:50:40 +0530

    updated the database details

--- config/settings.py
+++ config/settings.py
@@ -73,16 +73,29 @@ TEMPLATES = [
     },
 ]
 
+
+
 WSGI_APPLICATION = "config.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "snippetvault",
+        "USER": "snippetuser",
+        "PASSWORD": "StrongPassword@123",
+        "HOST": "35.239.25.25",
+        "PORT": "5432",
     }
 }
 
