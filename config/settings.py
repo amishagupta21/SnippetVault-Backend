d89be26309e636387bdd8c2c2afbcab9ff7fae29
Amisha Gupta <amisha.gupta672@gmail.com>
2026-04-20 15:30:24 +0530

    updated the database to the live

--- config/settings.py
+++ config/settings.py
@@ -81,14 +81,24 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "snippetvault-backend-db",
+        "USER": "snippetvault-backend-db",
+        "PASSWORD": "v^[@Hkb-8/eECK|Y",
+        "HOST": "104.198.174.76",
+        "PORT": "5432",
     }
 }
-
-
 
 
 # Password validation
