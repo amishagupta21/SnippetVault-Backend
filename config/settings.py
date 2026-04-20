b1c50cb052c06efcac56f7b15a7cb16fa3d625eb
Amisha Gupta <amisha.gupta672@gmail.com>
2026-04-20 15:18:15 +0530

    updated the setting env

--- config/settings.py
+++ config/settings.py
@@ -81,23 +81,14 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
-
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "snippetvault",
-        "USER": "snippetuser",
-        "PASSWORD": "StrongPassword@123",
-        "HOST": "35.239.25.25",
-        "PORT": "5432",
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
+
 
 
 # Password validation
